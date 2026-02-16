1c1bd8f51b9c76faff19038ee8f8d404cefa342a
marshall154 <tur84345@temple.edu>
2026-02-15 23:10:25 -0500

    Wrote code for using the nba_api player career stats data and testing listing the stats with pandas

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print ("Hello world!");
+from nba_api.stats.endpoints import playercareerstats
+import pandas as pd
+
+# Nikola Jokic
+career = playercareerstats.PlayerCareerStats(player_id='203999');
+# pandas data frames (optional: pip install pandas)
+
+# Get Nikola Jokic's total stats
+totals = career.season_totals_regular_season.get_data_frame();
+
+print (totals);
